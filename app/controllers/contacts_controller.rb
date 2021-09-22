f3c842d16f319a5d2b87d6bfee72f494283a5a9a
Jedidiah <bossjedi@philippinedev.com>
2021-09-22 23:39:58 +0800

    enable create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,4 +6,19 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :age, :number, :email)
+  end
 end
